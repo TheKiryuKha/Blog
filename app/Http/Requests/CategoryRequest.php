825bb2067ab7ee5e32d55d20df8237399c16b915
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-04-29 08:03:18 +0300

    refacto: requests authorization

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -7,15 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class CategoryRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        // Сделать норм авторизацию
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
--- app/Http/Requests/CommentRequest.php
+++ app/Http/Requests/CommentRequest.php
@@ -7,15 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class CommentRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        // сделать норм авторизацию
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
