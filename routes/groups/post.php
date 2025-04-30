1f4622e69869d41faa915a6c3d737920197b75ac
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-04-30 21:25:13 +0300

    refactor: post routes

--- routes/groups/post.php
+++ routes/groups/post.php
@@ -5,37 +5,37 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('admin_panel')->middleware('auth')->controller(PostController::class)->group(function(){
+Route::middleware('auth')->controller(PostController::class)->group(function(){
     
-    Route::get('/posts', 'index')
-        ->can('index', Post::class)
-        ->name('posts.index');
+    Route::prefix('admin_panel')->group(function(){
+        Route::get('/posts', 'index')
+            ->can('index', Post::class)
+            ->name('posts.index');
 
-    Route::post('/posts', 'store')
-        ->can('create', Post::class)
-        ->name('posts.store');
+        Route::post('/posts', 'store')
+            ->can('create', Post::class)
+            ->name('posts.store');
 
-    Route::get('/posts/create', 'create')
-        ->can('create', Post::class)
-        ->name('posts.create');
+        Route::get('/posts/create', 'create')
+            ->can('create', Post::class)
+            ->name('posts.create');
 
-    Route::get('/posts/{post}/edit', 'edit')
-        ->can('update', 'post')
-        ->name('posts.edit');
+        Route::get('/posts/{post}/edit', 'edit')
+            ->can('update', 'post')
+            ->name('posts.edit');
 
-    Route::patch('/posts/{post}', 'update')
-        ->can('update', 'post')
-        ->name('posts.update');
+        Route::patch('/posts/{post}', 'update')
+            ->can('update', 'post')
+            ->name('posts.update');
 
-    Route::delete('/posts/{post}', 'destroy')
-        ->can('delete', 'post')
-        ->name('posts.destroy');
-});
-
-Route::get('/posts/{post}', [PostController::class, 'show'])
-    ->middleware('auth')
+        Route::delete('/posts/{post}', 'destroy')
+            ->can('delete', 'post')
+            ->name('posts.destroy');
+    });
+    
+    Route::get('/posts/{post}', 'show')
     ->name('posts.show');
 
-Route::get('/posts/{post}/like', [PostController::class, 'like'])
-    ->middleware('auth')
+    Route::get('/posts/{post}/like', 'like')
     ->name('posts.like');
+});
